d3f8e556b3a372b9779b73602834e437df63c273
Sandeep Kumar <pksandeepkumar@gmail.com>
2016-07-01 19:09:39 +0530

    Asset folder added

---
+++ app/src/main/java/texus/autozoneuae/datamodels/CatData.java
@@ -0,0 +1,7 @@
+package texus.autozoneuae.datamodels;
+
+/**
+ * Created by sandeep on 1/7/16.
+ */
+public class CatData {
+}
---
+++ app/src/main/java/texus/autozoneuae/network/NetworkService.java
@@ -0,0 +1,30 @@
+package texus.autozoneuae.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by sandeep on 1/7/16.
+ */
+public class NetworkService {
+
+    public static String get(String url) {
+        try {
+            OkHttpClient client =  new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch ( Exception e) {
+            e.printStackTrace();;
+        }
+        return "";
+
+    }
+
+
+}
